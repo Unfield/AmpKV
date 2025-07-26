2188e32f94732c55444a2fda5c239e1a18fd4c56
Unfield <unfield@haschtable.de>
2025-07-26 19:24:51 +0200

    feat: Introduce AmpKVStorageMode for behavior configuration
    Adds an 'AmpKVStorageMode' option to 'AmpKVOptions', allowing users to define how the AmpKV instance will operate. This mode dictates the active drivers for cache and persistent storage.
    
    Supported modes:
    
    *   'AmpKVStorageModeDefault': (Default) Utilizes both Cache and Persistent Storage drivers.
    
    *   'AmpKVStorageModeCacheOnly': Operates solely with the Cache driver.
    
    *   'AmpKVStorageModeStoreOnly': Operates solely with the Persistent Storage driver.

--- pkg/embedded/embedded.go
+++ pkg/embedded/embedded.go
@@ -1,9 +1,12 @@
 package embedded
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
+	nilCacheDriver "github.com/Unfield/AmpKV/drivers/cache/nil"
+	nilStoreDriver "github.com/Unfield/AmpKV/drivers/store/nil"
 	"github.com/Unfield/AmpKV/internal/storage"
 	"github.com/Unfield/AmpKV/pkg/common"
 )
@@ -15,12 +18,91 @@ type AmpKV struct {
 	defaultCost int64
 }
 
+type AmpKVStorageMode uint8
+
+const (
+	AmpKVStorageModeDefault AmpKVStorageMode = iota
+	AmpKVStorageModeCacheOnly
+	AmpKVStorageModeStoreOnly
+)
+
+func (m AmpKVStorageMode) ToString() string {
+	switch m {
+	case AmpKVStorageModeDefault:
+		return "Default"
+	case AmpKVStorageModeCacheOnly:
+		return "CacheOnly"
+	case AmpKVStorageModeStoreOnly:
+		return "StoreOnly"
+	default:
+		return fmt.Sprintf("Unknown AmpKVStorageMode: %d", m)
+	}
+}
+
 type AmpKVOptions struct {
 	DefaultTTL  time.Duration
 	DefaultCost int64
+	Mode        AmpKVStorageMode
 }
 
-func NewAmpKV(cache storage.ICache, store storage.KVStore, options AmpKVOptions) (*AmpKV, error) {
+func NewAmpKV(cacheDriver, storeDriver any, options AmpKVOptions) (*AmpKV, error) {
+	var (
+		finalCache storage.ICache
+		finalStore storage.KVStore
+		ok         bool
+	)
+
+	switch options.Mode {
+	case AmpKVStorageModeDefault:
+		if cacheDriver == nil {
+			return nil, errors.New("cache driver cannot be nil for Default mode")
+		}
+		if storeDriver == nil {
+			return nil, errors.New("store driver cannot be nil for Default mode")
+		}
+
+		finalCache, ok = cacheDriver.(storage.ICache)
+		if !ok {
+			return nil, fmt.Errorf("cache driver does not implement storage.ICache interface for Default mode. Type: %T", cacheDriver)
+		}
+
+		finalStore, ok = storeDriver.(storage.KVStore)
+		if !ok {
+			return nil, fmt.Errorf("store driver does not implement storage.KVStore interface for Default mode. Type: %T", storeDriver)
+		}
+
+		return createAmpKV(finalCache, finalStore, options)
+
+	case AmpKVStorageModeStoreOnly:
+		if storeDriver == nil {
+			return nil, errors.New("store driver cannot be nil for StoreOnly mode")
+		}
+
+		finalStore, ok = storeDriver.(storage.KVStore)
+		if !ok {
+			return nil, fmt.Errorf("store driver does not implement storage.KVStore interface for StoreOnly mode. Type: %T", storeDriver)
+		}
+
+		return createAmpKV(&nilCacheDriver.NilCache{}, finalStore, options)
+
+	case AmpKVStorageModeCacheOnly:
+		if cacheDriver == nil {
+			return nil, errors.New("cache driver cannot be nil for CacheOnly mode")
+		}
+
+		finalCache, ok = cacheDriver.(storage.ICache)
+		if !ok {
+			return nil, fmt.Errorf("cache driver does not implement storage.ICache interface for CacheOnly mode. Type: %T", cacheDriver)
+		}
+
+		return createAmpKV(finalCache, &nilStoreDriver.NilStore{}, options)
+
+	default:
+		return nil, fmt.Errorf("unknown storage mode: %s", options.Mode.ToString())
+	}
+}
+
+func createAmpKV(cache storage.ICache, store storage.KVStore, options AmpKVOptions) (*AmpKV, error) {
 	if options.DefaultCost == 0 {
 		options.DefaultCost = 1
 	}
